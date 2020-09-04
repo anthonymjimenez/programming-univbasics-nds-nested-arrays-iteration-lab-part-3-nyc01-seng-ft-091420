229cd6505cd5b94d44e353e3246be207661db6a3
Anthony Jimenez <anthonyjim96@gmail.com>
2020-09-04 22:05:29 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -2,14 +2,12 @@ def join_nested_strings(src)
   # src will be an Array of Arrays of Strings and Integers
   # Combine all Strings present in the AoA into a single value and return it
   stringArr = []
-
   src.length.times do |x|
     src[x].length.times do |y|
       if src[x][y].is_a? String
-        puts 'wil you'
+        stringArr.push(src[x][y])
       end
     end
   end
+  stringArr.join('')
 end
-
-join_nested_strings([['I', 4], ['f', 5]])
